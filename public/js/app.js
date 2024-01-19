0ac8b31af31d2f8350e90b6c0920a40120562f54
kawtaradlany <adlanikawtar@gmail.com>
2024-01-19 11:50:34 +0100

    modaljs

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+let myLogin = document.querySelector("#login")
+console.log(myLogin);
+let mySignup = document.querySelector("#signup")
+console.log(mySignup);
+
+let myLoginContent = document.querySelector("#logincontent")
+console.log(myLoginContent);
+let mySignupContent = document.querySelector("#signupcontent")
+console.log(mySignupContent);
+myLogin.addEventListener('click', () => {
+    myLoginContent.classList.remove("d-none")
+    mySignupContent.classList.add("d-none")
+    console.log(myLoginContent);
+})
+mySignup.addEventListener('click', () => {
+    mySignupContent.classList.remove("d-none")
+    myLoginContent.classList.add("d-none")
+    console.log(mySignupContent);
+})
